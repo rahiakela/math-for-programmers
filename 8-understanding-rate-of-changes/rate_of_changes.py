1ed462ff1f228c5ee76ce502d1f9cf34bb27d8e9
Rahi Akela <rahi.akela@yahoo.com>
2021-01-14 18:12:20 +0530

    updated file

--- 8-understanding-rate-of-changes/rate_of_changes.py
+++ 8-understanding-rate-of-changes/rate_of_changes.py
@@ -1,4 +1,29 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
+
+def plot_function(f,tmin,tmax,tlabel=None,xlabel=None,axes=False, **kwargs):
+    ts = np.linspace(tmin,tmax,1000)
+    if tlabel:
+        plt.xlabel(tlabel,fontsize=18)
+    if xlabel:
+        plt.ylabel(xlabel,fontsize=18)
+    plt.plot(ts, [f(t) for t in ts], **kwargs)
+    if axes:
+        total_t = tmax-tmin
+        plt.plot([tmin-total_t/10,tmax+total_t/10],[0,0],c='k',linewidth=1)
+        plt.xlim(tmin-total_t/10,tmax+total_t/10)
+        xmin, xmax = plt.ylim()
+        plt.plot([0,0],[xmin,xmax],c='k',linewidth=1)
+        plt.ylim(xmin,xmax)
+
+
+def plot_volume(f,tmin,tmax,axes=False,**kwargs):
+    plot_function(f,tmin,tmax,tlabel="time (hr)", xlabel="volume (bbl)", axes=axes, **kwargs)
+
+
+def plot_flow_rate(f,tmin,tmax,axes=False,**kwargs):
+    plot_function(f,tmin,tmax,tlabel="time (hr)", xlabel="flow rate (bbl/hr)", axes=axes, **kwargs)
 
 
 def volume(t):
